bb090dea0fbb6eb3b11bb74cc26e1325b7556700
Faruk22571 <omorfaruk22571@gmail.com>
2026-09-13 08:01:31 +0600

    solve

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,11 +1,3 @@
-echo "# ASSIGNMENT-5" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/omorfaruk22/ASSIGNMENT-5.git
-git push -u origin main
-
 
 import React from 'react';
 
